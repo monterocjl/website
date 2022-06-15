76abe80e7b0c7ce75aefe530335c12b66ac40453
monterocjl <jmonteroc.91@gmail.com>
2022-06-15 18:52:13 -0500

    update

--- pages/api/leads.js
+++ pages/api/leads.js
@@ -10,51 +10,55 @@ const cors = initMiddleware(
 );
 
 export default async function handler(req, res) {
-  const notion = new Client({ auth: process.env.NOTION_TOKEN });
-  // Run cors
-  await cors(req, res);
+  try {
+    const notion = new Client({ auth: process.env.NOTION_TOKEN });
+    // Run cors
+    await cors(req, res);
 
-  // Get data submitted in request's body.
-  const body = req.body;
-  const new_lead = {
-    parent: {
-      database_id: process.env.NOTION_LEADS_DATABASE_ID,
-    },
-    properties: {
-      Email: {
-        title: [
-          {
-            text: {
-              content: body.identifier ? body.identifier : "",
-            },
-          },
-        ],
+    // Get data submitted in request's body.
+    const body = req.body;
+    const new_lead = {
+      parent: {
+        database_id: process.env.NOTION_LEADS_DATABASE_ID,
       },
-      Service: {
-        rich_text: [
-          {
-            text: {
-              content: body.product ? body.product : "",
+      properties: {
+        Email: {
+          title: [
+            {
+              text: {
+                content: body.identifier ? body.identifier : "",
+              },
             },
-          },
-        ],
-      },
-      Message: {
-        rich_text: [
-          {
-            text: {
-              content: body.message ? body.message : "",
+          ],
+        },
+        Service: {
+          rich_text: [
+            {
+              text: {
+                content: body.product ? body.product : "",
+              },
             },
-          },
-        ],
+          ],
+        },
+        Message: {
+          rich_text: [
+            {
+              text: {
+                content: body.message ? body.message : "",
+              },
+            },
+          ],
+        },
       },
-    },
-  };
+    };
 
-  await notion.pages.create(new_lead);
+    await notion.pages.create(new_lead);
 
-  res.writeHead(301, {
-    Location: "/",
-  });
-  res.end();
+    res.writeHead(301, {
+      Location: "/",
+    });
+    res.end();
+  } catch (error) {
+    console.log(error);
+  }
 }
